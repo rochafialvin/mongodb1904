d10ee1b186abac6e4c315baa0739c778aa6072f5
rochafialvin <rochafi.dev@gmail.com>
2022-02-03 10:29:50 +0700

    Update and delete query

--- query.js
+++ query.js
@@ -110,3 +110,110 @@ db.movies
   .find({ rated: { $ne: "TV-MA" } })
   .sort({ runtime: 1 })
   .limit(3);
+
+db.inventories.insertMany([
+  {
+    item: "canvas",
+    qty: 100,
+    size: { h: 28, w: 35.5, uom: "cm" },
+    status: "A",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    item: "journal",
+    qty: 25,
+    size: { h: 14, w: 21, uom: "cm" },
+    status: "A",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "mat",
+    qty: 85,
+    size: { h: 27.9, w: 35.5, uom: "cm" },
+    status: "A",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "mousepad",
+    qty: 25,
+    size: { h: 19, w: 22.85, uom: "cm" },
+    status: "P",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "notebook",
+    qty: 50,
+    size: { h: 8.5, w: 11, uom: "in" },
+    status: "P",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "paper",
+    qty: 100,
+    size: { h: 8.5, w: 11, uom: "in" },
+    status: "D",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "planner",
+    qty: 75,
+    size: { h: 22.85, w: 30, uom: "cm" },
+    status: "D",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "postcard",
+    qty: 45,
+    size: { h: 10, w: 15.25, uom: "cm" },
+    status: "A",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "sketchbook",
+    qty: 80,
+    size: { h: 14, w: 21, uom: "cm" },
+    status: "A",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+  {
+    item: "sketch pad",
+    qty: 95,
+    size: { h: 22.85, w: 30.5, uom: "cm" },
+    status: "A",
+    createdAt: new ISODate(),
+    updatedAt: new ISODate(),
+  },
+]);
+
+db.inventories.updateOne({ filter }, { $set: { new_data_here } });
+
+db.inventories.updateOne(
+  { _id: ObjectId("61fb4710a01d5468595461f6") },
+  { $set: { status: "Legendary", qty: 99 } }
+);
+
+db.inventories.updateOne({ qty: 25 }, { $set: { status: "Quarter" } });
+
+db.inventories.updateOne({ item: "paper" }, { $set: { "size.uom": "cm" } });
+
+db.movies.updateOne(
+  { title: "Bodyguard" },
+  { $set: { "genres.0": "Drama Korea" } }
+);
+
+db.inventories.updateOne({ item: "paper" }, { $set: { size: { uom: "cm" } } });
+
+db.inventories.updateMany({ qty: 25 }, { $set: { status: "Sold" } });
+
+db.inventories.deleteOne({ qty: 25 });
+
+db.inventories.deleteOne({ _id: ObjectId("61fb4710a01d5468595461fb") });
